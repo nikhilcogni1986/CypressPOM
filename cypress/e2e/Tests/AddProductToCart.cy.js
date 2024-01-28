561cde1c82c7d6cda7ae840850a2df2ce4dbb236
Nikhil <nikhilcogni@gmail.com>
2024-01-28 11:31:18 +0000

    Data Drive Adding Products to cart

--- cypress/e2e/Tests/AddProductToCart.cy.js
+++ cypress/e2e/Tests/AddProductToCart.cy.js
@@ -14,38 +14,73 @@ describe("Login to Ecommerce App", ()=>{
     const cartPage = new CartPage();
     const checkoutPage = new CheckoutPage();
 
+    before(()=>
+    {
+        cy.fixture('Login').then(data=>{
+            globalThis.loginData = data;
+        });
+
+        cy.fixture('productDetails').then(prodData=>{
+            globalThis.productDetails = prodData;
+        });    
+    })
+
     it("Login to App with Valid Credentials", ()=>
     {
-        let productName = "iPod Classic";
+
         cy.visit("https://naveenautomationlabs.com/opencart/index.php?route=account/login");
-        loginPage.elements.lblNewCustomer().should('be.visible');
-        loginPage.loginToApplication("nikhilrao@test.com","Password1234");
+        loginPage.
+                elements.lblNewCustomer().should('be.visible');
+        loginPage.
+                loginToApplication(globalThis.loginData[0].username,
+                                  globalThis.loginData[0].password);
 
-        landingPage.navigateToProductPage("MP3 Players","Show All MP3 Players");
-        Mp3Page.addProductToCart(productName);
+        landingPage.
+        navigateToProductPage(globalThis.productDetails.ProductCategory,
+                             globalThis.productDetails.SubCategory);
+                             
+        Mp3Page.
+               addProductToCart(globalThis.productDetails.ProductName);
 
-        landingPage.elements.btnCart().click();
-        landingPage.verifyProductInCart(productName);
-        landingPage.elements.btnViewCart().contains("View Cart").click();
+        landingPage.
+                elements.btnCart().click();
 
-        cy.url().should('include','?route=checkout/cart');
-        cartPage.elements.btnCheckout().click();
+        landingPage.
+                    verifyProductInCart(globalThis.productDetails.ProductName);
+        landingPage.
+                elements.
+                btnViewCart().contains("View Cart").click();
+
+        cy.url().
+                should('include','?route=checkout/cart');
+        cartPage.
+                elements.btnCheckout().click();
 
         //checkout process
-        checkoutPage.elements.radBtnNewAddr().check();
-        checkoutPage.fillBillingDetails();
-        checkoutPage.elements.btnBillingContinue().click();
+        checkoutPage.
+                elements.radBtnNewAddr().check();
+        checkoutPage.
+                fillBillingDetails();
+        checkoutPage.
+                elements.btnBillingContinue().click();
 
-        checkoutPage.elements.btnBillingContinue().click();
-        checkoutPage.elements.btnDeliveryDetailsContinue().click();
+        checkoutPage.
+                elements.btnBillingContinue().click();
+        checkoutPage.
+                elements.btnDeliveryDetailsContinue().click();
         
-        checkoutPage.elements.txtComments().type("ADD PRODUCT");
-        cy.wait(1000)
-        checkoutPage.elements.btnDeliveryMethodContinue().click({force:true});
+        checkoutPage.
+                elements.txtComments().type("ADD PRODUCT");
+                cy.wait(1000)
+        checkoutPage.
+                elements.btnDeliveryMethodContinue().click({force:true});
         
-        checkoutPage.elements.chlboxAgreeTerms().check();
-        checkoutPage.elements.btnPaymentContinue().click();
-        checkoutPage.elements.btnConfirmOrder().click();
+        checkoutPage.
+                elements.chlboxAgreeTerms().check();
+        checkoutPage.
+                elements.btnPaymentContinue().click();
+        checkoutPage.
+                elements.btnConfirmOrder().click();
 
         //Validate order success message
         cy.get("div[id='content'] h1").should('be.visible');
