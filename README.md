cf313167523926e5d6406b588e3820c5925070e7
Nikhil <nikhilcogni@gmail.com>
2024-01-28 11:16:48 +0000

    Data drive Login tests

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # CypressPOM
+Cypress-POM is based on Page Object Basic Model design pattern.
+
+https://github.com/nikhilcogni1986/CypressPOM.git
+https://www.npmjs.com/package/cypress-page-object-model
+
+How To Setup and Run ==> How To Use this framework ( Ready To Use )
+
+Step 1
+Install NodeJs https://nodejs.org/en/download/
+-Make Folder called "Cypress-POM-Framework" in any folder on your machine
+-Open Folder Using the VSCode editor or any IDE
+-Clone the repo using https://github.com/nikhilcogni1986/CypressPOM.git
+-Run command - npm install this will install all the dependencies required for this framework.
+-now hit npx cypress open
+-Now you can run tests 
+  - LoginTest.cy.js
+  - AddProductToCart.cy.js
+
--- cypress/e2e/Tests/LoginTest.cy.js
+++ cypress/e2e/Tests/LoginTest.cy.js
@@ -5,21 +5,38 @@ import LoginPage from "../../support/PageObjects/LoginPage";
 describe("Login to Ecommerce App", ()=>{
 
     const loginPage = new LoginPage();
-    
 
+    before(()=>{
+        cy.fixture('Login').then(data=>{
+            globalThis.loginData = data;
+        })
+    })
+    
     it("Login to App with Valid Credentials", ()=>
     {
         cy.visit("https://naveenautomationlabs.com/opencart/index.php?route=account/login");
-        loginPage.elements.lblNewCustomer().should('be.visible');
-        loginPage.loginToApplication("nikhilrao@test.com","Password1234");
+        loginPage.
+                elements.
+                lblNewCustomer().
+                should('be.visible');
+        loginPage.
+                loginToApplication(globalThis.loginData[0].username,
+                                  globalThis.loginData[0].password);
         loginPage.logout();
     }); 
 
     it("Login to App with Invalid Credentials", ()=>
     {
         cy.visit("https://naveenautomationlabs.com/opencart/index.php?route=account/login");
-        loginPage.elements.lblNewCustomer().should('be.visible');
-        loginPage.loginToApplication("nikhilrao@test.com","Password12345");
-        loginPage.elements.alrtErrorMsg().should('be.visible');
+        loginPage.
+                elements.
+                lblNewCustomer().
+                should('be.visible');
+        loginPage.
+                loginToApplication(globalThis.loginData[1].InvalidUsername,
+                                  globalThis.loginData[1].InvalidPassword);
+        loginPage.
+                elements.alrtErrorMsg()
+                .should('be.visible');
     });
 });
